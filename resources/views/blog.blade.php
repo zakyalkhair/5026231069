10d2d9b8d0cc166ca974aa9a93f7379c2eab2c67
Zaky <muhzakykhair@gmail.com>
2025-05-15 16:02:01 +0700

    Pertemuan 11

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tutorial Laravel - www.malasngoding.com</title>
+</head>
+<body>
+ 
+	<h3>www.malasngoding.com</h3>
+	<p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
+	<p>Ini adalah view blog. ada di route blog.</p>
+ 
+    <a href="satu">satu</a>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () { 
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+	return "<h1>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h1>";
+});
+
+Route::get('blog', function () {
+	return view('blog');
+});
+
+Route::get('satu', function () {
+	return view('Pertemuan 1');
+});
